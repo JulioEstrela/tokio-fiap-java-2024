945fc51898bc716dc473209538f9d7d62b7b0bf6
JulioEstrela <julioantunesestrela13@gmail.com>
2024-09-04 14:58:07 -0300

    refactor: remove Map to use switch case

--- JavaFiapTokio2024/src/logica/exercicios/aula06/Exercicio09.java
+++ JavaFiapTokio2024/src/logica/exercicios/aula06/Exercicio09.java
@@ -14,17 +14,33 @@ public class Exercicio09 {
 		System.out.println("3 - industria");
 		System.out.print("Insira seu tipo de cliente: ");
 		
+		/*
 		Map<Integer, Double> tipoClienteValor = new HashMap<Integer, Double>();
 		tipoClienteValor.put(1, 0.6);
 		tipoClienteValor.put(2, 0.48);
 		tipoClienteValor.put(3, 1.29);
+		*/
+		
 		
 		int tipoCliente = scanner.nextInt();
+		double valor = 0;
+		switch (tipoCliente) {
+		case 1:
+			valor = 0.6;
+			break;
+		case 2:
+			valor = 0.48;
+			break;
+		case 3:
+			valor = 1.29;
+			break;
+		}
 		
-		System.out.print("Insira a quantidade de KW/h utilizados: ");
+		System.out.print("Insira a quantidade de kWh utilizados: ");
 		int consumoHoras = scanner.nextInt();
 		
-		System.out.println("Valor total da conta: " + tipoClienteValor.get(tipoCliente) * consumoHoras);
+		//System.out.println("Valor total da conta: " + tipoClienteValor.get(tipoCliente) * consumoHoras);
+		System.out.println("Valor total da conta: " + valor * consumoHoras);
 	}
 
 }
--- JavaFiapTokio2024/src/logica/exercicios/aula06/Exercicio11.java
+++ JavaFiapTokio2024/src/logica/exercicios/aula06/Exercicio11.java
@@ -14,12 +14,14 @@ public class Exercicio11 {
 		int codigoCarga = scanner.nextInt();
 		double precoPorQuilo;
 		
+		/*
 		Map<Integer, Double> codigoEstadoImposto = new HashMap<Integer, Double>();
 		codigoEstadoImposto.put(1, 0.35);
 		codigoEstadoImposto.put(2, 0.25);
 		codigoEstadoImposto.put(3, 0.15);
-		codigoEstadoImposto.put(4, 0.5);
+		codigoEstadoImposto.put(4, 0.05);
 		codigoEstadoImposto.put(5, 0.0);
+		*/
 		
 		if (codigoCarga <= 20) {
 			precoPorQuilo = 100;
@@ -29,9 +31,26 @@ public class Exercicio11 {
 			precoPorQuilo = 340;
 		}
 		
+		double taxaImposto = 0;
+		switch (codigoEstado) {
+			case 1:
+				taxaImposto = 0.35;
+				break;
+			case 2:
+				taxaImposto = 0.25;
+				break;
+			case 3:
+				taxaImposto = 0.15;
+				break;
+			case 4:
+				taxaImposto = 0.05;
+				break;
+		}
+		
 		double quilosCarga = toneladasCarga * 1000;
 		double valorSemImposto = quilosCarga * precoPorQuilo;
-		double valorImposto = codigoEstadoImposto.get(codigoEstado) * valorSemImposto;
+		// double valorImposto = codigoEstadoImposto.get(codigoEstado) * valorSemImposto;
+		double valorImposto = taxaImposto * valorSemImposto;
 		
 		System.out.println("Carga do caminhao em quilos: " + quilosCarga);
 		System.out.println("Preco da carga: " + valorSemImposto);
