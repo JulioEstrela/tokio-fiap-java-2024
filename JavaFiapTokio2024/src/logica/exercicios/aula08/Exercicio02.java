619b82229aa3d89ec7141027d36e22bfcc423dcb
JulioEstrela <julioantunesestrela13@gmail.com>
2024-09-05 18:01:27 -0300

    fix: aula08 Exercicio02

--- JavaFiapTokio2024/src/logica/exercicios/aula08/Exercicio02.java
+++ JavaFiapTokio2024/src/logica/exercicios/aula08/Exercicio02.java
@@ -6,10 +6,10 @@ public class Exercicio02 {
 	public static void main(String[] args) {
 		Calendar calendar = Calendar.getInstance();
 
-		// TODO se for terça feira
-		// calendar.get(Calendar.DAY_OF_WEEK) retorna o dia errado?
-		if (calendar.get(Calendar.DAY_OF_WEEK) == calendar.get(Calendar.THURSDAY)) {
+		if (calendar.get(Calendar.DAY_OF_WEEK) == calendar.get(Calendar.TUESDAY)) {
 			System.out.println("Hoje tem feira de rua");
+		} else {
+			System.out.println("Hoje nao tem feira de rua");
 		}
 	}
 }
